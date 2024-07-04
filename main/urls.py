ec3b9ccc8db12b345786a52ea74ff6a097383985
Joseph Chan <joschan@yahoo.co.uk>
2024-07-04 15:34:33 +0000

    enable serving of static files

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     #path('', views.PostList.as_view(template_name="main/booklist.html"), name='main'),
-    path('', views.PostList.as_view(), name='main'),
+    path('', views.PostList.as_view(), name='home'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,4 +5,6 @@ from .models import booklist
 # Create your views here.
 class PostList(generic.ListView):
     queryset = booklist.objects.all()
-    template_name = "booklist.html"
+    #template_name = "booklist.html"
+    template_name = "main/index.html"
+    paginate_by = 6
